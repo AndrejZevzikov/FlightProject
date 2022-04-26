48180b351557d3c6bc3093555793d3e9434b06f0
Andrej Zevzikov <andrejzevz13@gmail.com>
2022-04-26 15:35:26 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
